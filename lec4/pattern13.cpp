7444d720e5c33357941c72f01c72f141b915b41c
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2022-06-19 15:30:13 +0530

    another one

---
+++ lec4/pattern13.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main (
+
+)
+{
+    int n ;
+    cin>>n;
+    int row=1;
+    while (row<=n)
+    {
+        int col =1;
+        while (col<=n)
+        {
+            char cal='A'+col-1;
+
+            cout<<cal;
+            col=col+1;
+            
+        }
+        
+        cout<<endl;
+        row=row+1;
+    }
+    
+}
--- lec4/tempCodeRunnerFile.cpp
+++ lec4/tempCodeRunnerFile.cpp
@@ -1,23 +1,18 @@
 #include<iostream>
 using namespace std;
-int main ()
-{
-    int n ;
+int main(){
+    int n;
     cin>>n;
     int i =1;
-    int count =1;
     while (i<=n)
     {
-        int j =1;
+        int j=1;
         while (j<=i)
         {
-            cout<<"*";
-            count=count+1;
+            cout<<i-j+1<<" ";
             j=j+1;
         }
         cout<<endl;
         i=i+1;
     }
-    
 }
-
