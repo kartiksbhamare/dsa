37d464d9e1eb0002d6e7c424a162f8b958458326
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2022-07-11 15:17:20 +0530

    increment and left shift

---
+++ lec5/increment_and decre.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int i =4;
+    int a = i++;
+    int b= ++i;
+    cout<<i<<endl;
+    cout<<a<<endl;
+    cout<<b<<endl;
+
+
+}
---
+++ lec5/right_shift.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main (){
+    cout<<(16>>1)<<endl;
+    cout<<(19>>2)<<endl;
+}
