700207e6d0dff6ab85302b14532f1675d963f4f2
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2022-07-11 15:57:36 +0530

    #	modified:   .vscode/settings.json #	new file:   lec5/for_loop.cpp #	new file:   lec5/for_loop.exe #	deleted:    lec5/increment_and decre.cpp #	new file:   lec5/increment_and_decre.cpp #	new file:   lec5/increment_and_decre.exe #	new file:   lec5/sum_for.cpp

---
+++ lec5/for_loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a;
+    int i=0 ;
+    cout<<"enter the value= ";
+    cin>>a;
+    for ( i = 1; i<=a; i++)
+    {
+        cout<<i<<endl;
+
+    }
+    
+}
