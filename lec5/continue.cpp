493aaf8189379485e032a47a2162dd4e9c632e83
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2022-08-07 15:24:50 +0530

    kk

---
+++ lec5/continue.cpp
@@ -0,0 +1,15 @@
+#include<iostream>;
+using namespace std;
+int main(){
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<"hi"<<endl;
+         cout<<"hh"<<endl;
+          cout<<"hmm"<<endl;
+           cout<<"dd"<<endl;
+           continue;
+           cout<<"areh yrr"<<endl;
+    }
+    
+
+}
--- lec5/find_prime.cpp
+++ lec5/find_prime.cpp
@@ -3,5 +3,28 @@ using namespace std;
 int main(
 
 ){
+    int n ;
+    cout<<"enter the value of n "<<endl;
+    cin>>n;
+    bool isprime=1;
+    for ( int i =2 ; i < n; i++)
+    {
+        if (n%i==0)
+        {
+            isprime=0;
+            break;
+        }
+    }
+    if (isprime==0)
+    {
+        cout<<"n is not a prime number"<<endl;
+
+    }
+    else{
+        cout<<"n is a prime number"<<endl;
+
+    }
     
+    
+
 }
