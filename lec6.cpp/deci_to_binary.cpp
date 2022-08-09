4cebdddbc2ac4f64453136291f838f6e590ade76
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2022-08-09 15:11:34 +0530

    6

--- lec6.cpp/deci_to_binary.cpp
+++ lec6.cpp/deci_to_binary.cpp
@@ -5,13 +5,13 @@ int main(){
     int n ;
     cin>>n;
     int ans=0;
-    int i;
+    int i=0;
     while (n!=0){
         int bit=n&1;
-        ans=(bit*pow(10,i))+ans;
-        i++;
+        ans=(bit * pow(10,i))+ans;
         n=n>>1;
+        i++;
 
     }
-cout<<ans<<endl;
+    cout<<ans<<endl;
 }
--- lec6.cpp/negative_number_to_binary.cpp
+++ lec6.cpp/negative_number_to_binary.cpp
@@ -5,13 +5,12 @@ int main(){
     int n ;
     cin>>n;
     int ans=0;
-    int i;
+    int i=0;
     while (n!=0){
         int bit=n&1;
         ans=(bit * pow(10,i))+ans;
         n=n>>1;
         i++;
-
     }
     cout<<ans<<endl;
 }
