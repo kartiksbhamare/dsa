4cea5ce343e1f10a0c66326a45fcf1c1ee81f3d5
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2022-08-18 20:03:40 +0530

    before grind

---
+++ lec_8/hw1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int data(int a ){
+    int ans=3*(a)+7;
+    cout<<ans<<endl;
+
+}
+int main (){
+    int b;
+    cin>>b;
+    data(b);
+    
+
+
+}
--- lec_8/prime.cpp
+++ lec_8/prime.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
-
+bool prime(int n  ){
+    for (int  i = 2; i <n ; i++)
+    { if (n%i==0)
+    {
+       return 0;
+    }
+    
+        else{
+            return 1;
+        }
+    }
+    
+}
 int main (){
+    int a ;
+    cin>>a;
+    if (prime(a)){
+        cout<<"prime" <<endl;
+    }
+    else {
+        cout<<"not a prime"<<endl;
+    }
 
 }
