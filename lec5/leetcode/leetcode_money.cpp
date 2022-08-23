0d313bd9e5ffa2ca885f90e2d93fb5d4dd5494ff
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2022-08-23 21:59:53 +0530

    .

--- lec5/leetcode/leetcode_money.cpp
+++ lec5/leetcode/leetcode_money.cpp
@@ -1,20 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
   int n;
-  cin>>n;
-       int week=n/7;
-        int min=week*28;
-        int left=n-min;
-        int i=0;
-       int sum;
-      int total;
-        for(int i=week+1;i<left;i++){
-            
-            sum=sum+i;
-            total=sum+min;
-           
-        }
-         cout<<total;
-        
+  cin >> n;
+  int week = n / 7;
+  int min = week * 28;
+  int left = n - min;
+  int i = 0;
+  int sum = 0;
+  int total=0;
+  for (int i = week + 1; i < left; i++)
+  {
+
+    sum += i;
+  }
+  cout << sum;
+  total = week + sum;
+  cout << total;
 }
