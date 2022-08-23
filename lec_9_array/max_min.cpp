e01ef349f3512f22325b22d9b10271d4fb112a5a
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2022-08-23 22:00:06 +0530

    array

---
+++ lec_9_array/max_min.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void(int arr[],int )
+int main () {
+    int arr[6]={8,5,2,5,8,82};
+    for (int i = 0; i <7; i++)
+    { 
+        if (arr[i]> arr[0])
+        {
+            int max=arr[i];
+        }
+        
+        
+    }
+   
+     
+
+}
