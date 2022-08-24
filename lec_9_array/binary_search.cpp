601f0ce1c60ffc7780aea6cc4f4233e2fe952146
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2022-08-25 00:32:38 +0530

    grinding

---
+++ lec_9_array/binary_search.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void reverse (int arr[],int n){
+
+}
+void print(int arr[],int n ){
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cout<<arr[i]<<" ";
+    }
+    
+}
+int main (){
+    int arr[100];
+    int size ;
+    cin>> size;
+    cout<<print(arr)<<endl;
+     
+}
