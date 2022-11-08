9e93149941cda4f00ca350eec87d1dc21fd947bb
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2022-11-08 15:41:47 +0530

    day 3

--- LEC_18_insertio_sort/insertion.cpp
+++ LEC_18_insertio_sort/insertion.cpp
@@ -1,7 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 void insertion(int arr[],int n){
+ for (int i =1; i < n; i++)
+ {
+    int temp=arr[i];
+    int  j = i-1;
+    for (; j>=0; j--)
+    {
+       if (arr[j]>temp)
 
+       {
+        arr[j]=arr[j+1];
+       }
+       else{
+        break;
+    }
+    arr[j+1]=temp;
+    } 
+ }
+ 
 }
 void printarray(int arr[],int n ){
     for (int i = 0; i < n; i++)
@@ -15,5 +32,13 @@ void printarray(int arr[],int n ){
 int main (){
 int arr [100];
 int n ;
-cin>>
+cin>>n;
+for (int i = 0; i < n; i++)
+{
+    cin>>arr[i];
+
+}
+insertion( arr, n );
+printarray(arr, n);
+
 }
