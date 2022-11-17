ce32df60ce61c89f4d48ab7bee4155326ab9d657
kartik_bhamare <82748419+kartiksbhamare@users.noreply.github.com>
2022-11-17 18:57:47 +0530

    2d array

---
+++ lec_23/tempCodeRunnerFile.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int arrr[3][4];
+    for(int i =0;i<3;i++){
+        for (int j = 0; i < 4; j++)
+        {
+            cin>>arrr[3][4];
+        }
+        
+    }
+    for(int i =0;i<3;i++){
+        for (int j = 0; i < 4; j++)
+        {
+            cout<<arrr[3][4]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
